ef035024855b892a0f6a926d88ea544371aaaa4e
Andrew Kojemiakin <93680493+MrKojemiakin@users.noreply.github.com>
2021-12-27 18:47:23 +0300

    check
    check

---
+++ SortAlgorithms/SortAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortAlgorithms
+{
+    internal class ListSorting
+    {
+        var rnd = new Random();
+        var array = new double[(int)1e6];
+            for (int i = 0; i<array.Length; i++)
+        {
+            array[i] = rnd.NextDouble()* 100;
+        }
+
+}
+    
+}
